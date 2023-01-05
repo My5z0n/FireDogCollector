05c47ae7cf87991977874f7fd1143a6251bfa123
Szymon Nagel <szmnnagel@gmail.com>
2023-01-05 09:25:14 +0100

    Modify sql structure

--- ops/create_tables.sql
+++ ops/create_tables.sql
@@ -1,17 +1,16 @@
-CREATE TABLE FireDogTraces.dogdig
+CREATE TABLE FireDogTraces.traces
 (
     trace_id String,
     paths String,
-    pathsArray Array(Array(Tuple(span_name String, span_id String))),
-    firedog_test1 String,
-    firedog_test2 String,
-    firedog_test3 String
+    paths_array Array(Array(Tuple(span_name String, span_id String))),
+    start_time DATETIME,
+    json_spans String
 )
 ENGINE = MergeTree()
 PRIMARY KEY (trace_id, paths);
 
 SET allow_experimental_object_type=1;
-CREATE TABLE FireDogTraces.traces
+CREATE TABLE FireDogTraces.spans
 (
     trace_id String,
     span_id String,
@@ -27,18 +26,18 @@ PRIMARY KEY (trace_id, span_id);
 CREATE TABLE FireDogTraces.predictions
 (
     trace_id String,
-    anomaly_detected Bool,
+    anomaly_detected Nullable(Bool),
     span_name String,
     span_id String,
     expected_span_name String
 )
 ENGINE = MergeTree()
 PRIMARY KEY (trace_id,anomaly_detected);
+
 ----------------------------------------------------
 CREATE MATERIALIZED VIEW mqtraceprocess
 ENGINE = RabbitMQ SETTINGS rabbitmq_address = 'amqp://guest:guest@some-rabbit:5672/',
                             rabbitmq_exchange_name = 'newSpanToProcessNotification',
                             rabbitmq_format = 'JSONEachRow',
                             rabbitmq_exchange_type = 'fanout'
-AS SELECT trace_id from dogdig;
------------------------------------------------------
+AS SELECT trace_id from traces;
